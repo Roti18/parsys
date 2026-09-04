44d8da0b20a6f7819514a505ff13baf371019d9c
Roti18 <akuronif@gmail.com>
2026-09-04 17:52:17 +0700

    refactor: wrap searchInput with Input component

---
+++ src/lib/components/ui/searchInput.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Input from './Input.svelte';
+
+	let { type = 'search', value = $bindable(''), placeholder = 'Cari...' } = $props();
+</script>
+
+<Input {type} bind:value {placeholder} />
+
